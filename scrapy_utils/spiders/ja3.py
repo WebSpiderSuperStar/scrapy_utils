dd8d47c6dc182ba3c21cca14cbecc4683ecc033a
Payne <wuzhipeng1289690157@gmail.com>
2021-12-28 00:01:29 +0800

    change yeild to yeild from

--- scrapy_utils/spiders/ja3.py
+++ scrapy_utils/spiders/ja3.py
@@ -7,6 +7,7 @@ class Ja3Spider(scrapy.Spider):
 
     def start_requests(self):
         # for _ in range(5):
+        #     scrapy.Request(url=self.start_urls[0], dont_filter=True)
         yield from [scrapy.Request(url=self.start_urls[0], dont_filter=True) for _ in range(5)]
 
     def parse(self, response, **kwargs):
