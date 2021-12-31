8d816e85d9540a0c037fce2dd364191e2fd3a25a
Payne <wuzhipeng1289690157@gmail.com>
2021-12-31 14:03:46 +0800

    commit

--- scrapy_utils/spiders/ja3.py
+++ scrapy_utils/spiders/ja3.py
@@ -6,8 +6,15 @@ class Ja3Spider(scrapy.Spider):
     start_urls = ['https://ja3er.com/json']
 
     def start_requests(self):
-        for _ in range(5):
-            yield scrapy.Request(url=self.start_urls[0], dont_filter=True)
+        """start request
+        :return:
+        """
+        # for _ in range(5):
+        #     yield scrapy.Request(url=self.start_urls[0], dont_filter=True)
+        yield from [
+            scrapy.Request(url=self.start_urls[0], dont_filter=True)
+            for _ in range(10)
+        ]
 
     def parse(self, response, **kwargs):
         """parse response
